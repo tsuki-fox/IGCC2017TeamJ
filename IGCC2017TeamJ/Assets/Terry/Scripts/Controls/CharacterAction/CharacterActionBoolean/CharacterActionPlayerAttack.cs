6e4ca59bf904dd28615f973f8053d52b3d961627
Terry Lim <lnxterry@gmail.com>
2017-09-14 16:45:39 +0900

    Added Win Event. Added Player Attack. Fixed Compile Errors.

---
+++ IGCC2017TeamJ/Assets/Terry/Scripts/Controls/CharacterAction/CharacterActionBoolean/CharacterActionPlayerAttack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterActionPlayerAttack : CharacterActionBoolean {
+
+    [SerializeField]
+    GameObject powerCord;
+
+	// Use this for initialization
+	void Start () {
+        powerCord = GameObject.Instantiate(powerCord);
+        powerCord.transform.SetParent(gameObject.transform);
+        powerCord.transform.localPosition = new Vector3();
+        powerCord.transform.localRotation = new Quaternion();
+        powerCord.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsActionActive()) {
+            powerCord.SetActive(true);
+            DeactivateAction();
+        }
+	}
+
+    public override void ResetAction() {
+        powerCord.SetActive(false);
+    }
+
+}
---
+++ IGCC2017TeamJ/Assets/Terry/Scripts/Gameplay/PowerCord.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using FlowAI;
+
+public class PowerCord : MonoBehaviour {
+
+    public float duration = 1.0f;
+    public float timer = 0.0f;
+    public float distance = 5.0f;
+    public List<string> hackableTags;
+
+    private bool isHacking = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnEnable() {
+        timer = 0.0f;
+        isHacking = false;
+    }
+
+    void UpdateLocalScale() {
+        Vector3 scale = new Vector3(1.0f, 1.0f, 1.0f);
+        timer += Time.deltaTime;
+        scale.z = Mathf.Sin((timer / duration) * Mathf.PI) * distance;
+        transform.localScale = scale;
+    }
+
+    void UpdateLocalPosition() {
+        Vector3 position = new Vector3(0.0f, 0.0f, 0.0f);
+        position.z = transform.localScale.z * 0.5f;
+        transform.localPosition = position;
+    }
+
+    void UpdateHackingCheck() {
+        RaycastHit[] result = Physics.RaycastAll(transform.position, transform.forward, transform.localScale.z);
+        for (int i = 0; i < result.Length; ++i) {
+            Collider hitCollider = result[i].collider;
+            GameObject hitGameObject = hitCollider.gameObject;
+
+            for (int j = 0; j < hackableTags.Count; ++j) {
+                if (hitGameObject.tag == hackableTags[j]) {
+                    FlowAIHolder flowAIHolder = hitGameObject.GetComponent<FlowAIHolder>();
+                    if (flowAIHolder == null) {
+                        break;
+                    }
+
+                    {
+                        // Can't hack if they can see us.
+                        EnemyControl_Patrol enemyControlPatrol = hitGameObject.GetComponent<EnemyControl_Patrol>();
+                        if (enemyControlPatrol != null && enemyControlPatrol.CanSeePlayer()) {
+                            break;
+                        }
+                    }
+
+                    {
+                        // Check Turret here.
+                    }
+
+                    // Do hacking here.
+                    Debug.Log("Hacked");
+
+                    FlowAIVisualizer visualizer = FindObjectOfType<FlowAI.FlowAIVisualizer>();
+                    Assert.AreNotEqual(visualizer, null);
+                    visualizer.target = flowAIHolder;
+
+                    visualizer.BeginHacking(0.0f);
+
+                    gameObject.SetActive(false);
+
+                    break;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isHacking) {
+            return;
+        }
+
+        UpdateLocalScale();
+        UpdateLocalPosition();
+        UpdateHackingCheck();
+
+        if (timer > duration) {
+            gameObject.SetActive(false);
+        }
+	}
+
+}
