6c9179382f4c0b1f60dfc1efaf4b6a853aebba40
Terry Lim <lnxterry@gmail.com>
2017-09-15 13:14:36 +0900

    Added NoFly to stop player from flying away.

---
+++ IGCC2017TeamJ/Assets/Terry/Scripts/Gameplay/NoFly.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoFly : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+        if (rigidbody == null) {
+            return;
+        }
+
+        if (rigidbody.velocity.y > 0.0f) {
+            Vector3 newVelocity = rigidbody.velocity;
+            newVelocity.y = 0.0f;
+            rigidbody.velocity = newVelocity;
+        }
+	}
+
+}
